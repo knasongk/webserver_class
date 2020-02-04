27945f7fb37304461a58e7d6b34b75ba8acb3ba9
Ken <knasongk@yahoo.com>
2020-02-04 00:29:49 -0800

    revised unit test

--- week2/test/myfunc.spec.js
+++ week2/test/myfunc.spec.js
@@ -2,14 +2,33 @@ import { expect } from 'chai';
 import getTourById from '../src/tours.js';
 
 describe('MyModule', () => {
-   describe('myFunc (getTourById) happy path', () => {
-       it('return object info for tour#1 when given 1', () => {
-	   expect(getTourById(1)).to.equal({ id:1, description:"boat ride along the main river", cost:"$100" });
-       });
+   describe('myFunc (getTourById) happy path for input value 1', () => {
+       it('check cost for tour#1 when given 1', () => {
+	       const tour = getTourById(1);
+	       expect(tour.cost).to.equal("$100") });
+       it('check description for tour#1 when given 1', () => {
+	       const tour = getTourById(1);
+	       expect(tour.description).to.equal("boat ride along the main river") });
+       it('check id for tour#1 when given 1', () => {
+	       const tour = getTourById(1);
+	       expect(tour.id).to.equal(1) });
    });
-   describe('myFunc (getTourById) happy path', () => {
-       it('return object info for tour#2 when given 2', () => {
-	   expect(getTourById(2)).to.equal({ id:2, description:"temple excursion in Songkhla province", cost:"$250" });
-       });
+
+   describe('myFunc (getTourById) happy path for input value 2', () => {
+       it('check cost for tour#1 when given 2', () => {
+	       const tour = getTourById(2);
+	       expect(tour.cost).to.equal("$250") });
+       it('check description info for tour#1 when given 2', () => {
+	       const tour = getTourById(2);
+	       expect(tour.description).to.equal("temple excursion in Songkhla province") });
+       it('check id for tour#1 when given 2', () => {
+	       const tour = getTourById(2);
+	       expect(tour.id).to.equal(2) });
+   });
+
+   describe('myFunc (getTourById) unhappy path for input value 3', () => {
+       it('check for undefined tour when given 3', () => {
+	       const tour = getTourById(3);
+	       expect(tour).to.equal(undefined) });
    });
 });
