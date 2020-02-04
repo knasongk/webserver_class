5ecb80e434da05a68a371457f6a518ec8637e688
Ken <knasongk@yahoo.com>
2020-02-03 21:27:47 -0800

    week2 src update

--- week2/src/index.js
+++ week2/src/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express();
-const staticRoute = express.static('public');
+//const staticRoute = express.static('public');
+const staticRoute = express.static(__dirname + '/public');
 app.use('/', staticRoute);
 
 var port = process.env.PORT || 8080;
