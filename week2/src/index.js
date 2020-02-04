9f0138875541d4476759c56ae27974ac83e1ea22
Ken <knasongk@yahoo.com>
2020-02-03 22:37:31 -0800

    add tours.js

--- week2/src/index.js
+++ week2/src/index.js
@@ -1,12 +1,24 @@
 import express from 'express'
 import cors from 'cors'
+import { getTourById } from './tours.js';
 
 const app = express();
-//const staticRoute = express.static('public');
+
 const staticRoute = express.static(__dirname + '/public');
 app.use('/', staticRoute);
 
 var port = process.env.PORT || 8080;
+
+const tourPackage = (req, res) => {
+	const { id } = req.params || {};
+	console.log("id = ", id);
+
+	const tour = getTourById(parseFloat(id));
+	res.json(tour);
+};
+
+app.get('/api/tour/:id', tourPackage); 
+
 
 app.listen(port, function() {
 	console.log(`listening on port ${port}`)
---
+++ week2/src/tours.js
@@ -0,0 +1,14 @@
+const tours = [{
+	id: 1,
+	description: "boat ride along the main river",
+	cost: 100,
+   },
+   {
+	id: 2,
+	description: "temple excursion in Songkhla province",
+	cost: 250,
+	   
+   }]; 
+
+export const getTourById = id =>
+	tours.find(tour => tour.id === id);
