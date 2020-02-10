486799686148b9700c7886edb13386e2e54aeeba
Ken <knasongk@yahoo.com>
2020-02-09 21:37:13 -0800

    add more tables to travel database

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (id serial, display_name varchar(100), username varchar(100), email varchar(100), created_on timestamp, last_login timestamp);
-CREATE TABLE IF NOT EXISTS destinations (id serial, city varchar(100), country varchar(100), language char(100));
-CREATE TABLE IF NOT EXISTS preferences (id serial, mood char(100), age_group char(100));
-CREATE TABLE IF NOT EXISTS themes (id serial, activity varchar(100), description text);
-CREATE TABLE IF NOT EXISTS tour_types (id serial, type char(100), budget integer, tour_description text);
+CREATE TABLE IF NOT EXISTS destinations (id serial, city varchar(50), country varchar(50), language varchar(50));
+CREATE TABLE IF NOT EXISTS preferences (id serial, mood varchar(100));
 CREATE TABLE IF NOT EXISTS city_preferences (id serial, city_id integer, preference_id integer);
+CREATE TABLE IF NOT EXISTS age_groups (id serial, category varchar(100), min_age integer, max_age integer);
+CREATE TABLE IF NOT EXISTS age_preferences(id serial, age_id integer, preference_id integer, theme_id integer);
+CREATE TABLE IF NOT EXISTS budgets (id serial, category varchar(100), min_cost money, max_cost money);
+CREATE TABLE IF NOT EXISTS themes (id serial, activity varchar(100), description text, budget_id integer, preference_id integer);
 
--- sql/seed.sql
+++ sql/seed.sql
@@ -2,6 +2,7 @@ INSERT INTO users (display_name, username, email, created_on, last_login) VALUES
    ('knasongk', 'Ken Nasongkhla', 'knasongk@yahoo.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
    ('madnat', 'Natalie Shannon', 'madnatj1@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
    ('tnguyen', 'Tam Nguyen', 'tytntam@gmail.com', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP); 
+
 INSERT INTO destinations (city, country, language) VALUES
    ('Melbourne', 'Australia', 'English'),
    ('Sydney', 'Australia', 'English'),
@@ -12,3 +13,53 @@ INSERT INTO destinations (city, country, language) VALUES
    ('Berlin', 'Germany', 'German'),
    ('Hamburg', 'Germany', 'German'),
    ('Munich', 'Germany', 'German');
+
+INSERT INTO preferences (mood) VALUES
+   ('slow pace'),
+   ('adventure'),
+   ('family fun'),
+   ('shopping'),
+   ('beach'),
+   ('sport'),
+   ('food & drink'),
+   ('city highlights'),
+   ('day trip'),
+   ('half day trip'),
+   ('introduction to city'),
+   ('nightlife'),
+   ('outdoors'),
+   ('cultural'),
+   ('tourist spots'),
+   ('off tourist trail');
+
+INSERT INTO age_groups (category, min_age, max_age) VALUES
+   ('children', 0, 18),
+   ('teens', 12, 18),
+   ('young adults', 19, 26),
+   ('mature adults', 27, 45),
+   ('middle ages', 46, 56),
+   ('older adults', 57, 66),
+   ('retirees', 67, 100);
+
+
+INSERT INTO age_preferences (age_id, preference_id, theme_id) VALUES 
+   (1, 2, 1),
+   (1, 2, 4),
+   (2, 3, 4),
+   (7, 7, 3),
+   (1, 2, 4);
+
+INSERT INTO budgets (category, min_cost, max_cost) VALUES
+   ('low', 20.0, 99.0),
+   ('medium', 100.0, 399.0),
+   ('high', 400.0, 999.0),
+   ('expensive', 1000.0, 10000.0);
+
+INSERT INTO themes (activity, description, budget_id, preference_id) VALUES
+   ('big bus tour', 'get on and off big city bus', 1, 1),
+   ('galaria shopping', 'go shopping all malls', 3, 2),
+   ('night clubs', 'go clubbing', 3, 3),
+   ('soccer match', 'go to local club team match', 2, 4);
+
+INSERT INTO city_preferences (preference_id, city_id) VALUES
+   (2,1), (3,2), (3,3), (3,4), (4,5), (6,7), (8,8);
