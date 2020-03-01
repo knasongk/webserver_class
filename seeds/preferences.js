0fa596f9d927648956190187d491f8fc12e714eb
Ken Nasongkhla <knasongk@yahoo.com>
2020-02-29 23:31:47 -0800

    add more database data

--- seeds/preferences.js
+++ seeds/preferences.js
@@ -20,7 +20,51 @@ exports.seed = async knex => {
 	},
 	{
 	   id: 5,
+	   mood: 'beach'
+	},
+	{
+	   id: 6,
 	   mood: 'sport'
+	},
+	{
+	   id: 7,
+	   mood: 'food & drink'
+	},
+	{
+	   id: 8,
+	   mood: 'city highlights'
+	},
+	{
+	   id: 9,
+	   mood: 'day trip'
+	},
+	{
+	   id: 10,
+	   mood: 'half day trip'
+	},
+	{
+	   id: 11,
+	   mood: 'city excursion'
+	},
+	{
+	   id: 12,
+	   mood: 'nightlife'
+	},
+	{
+	   id: 13,
+	   mood: 'outdoors'
+	},
+	{
+	   id: 14,
+	   mood: 'cultural'
+	},
+	{
+	   id: 15,
+	   mood: 'tourist spots'
+	},
+	{
+	   id: 16,
+	   mood: 'off tourist trail'
 	}
     ]);
  
@@ -28,6 +72,13 @@ exports.seed = async knex => {
     await knex('city_preferences').insert([
 	{ preference_id: 2, city_id: 1},
 	{ preference_id: 2, city_id: 3},
-	{ preference_id: 3, city_id: 1}
+	{ preference_id: 4, city_id: 5},
+	{ preference_id: 9, city_id: 7},
+	{ preference_id: 5, city_id: 9},
+	{ preference_id: 7, city_id: 7},
+	{ preference_id: 15, city_id: 10},
+	{ preference_id: 14, city_id: 12},
+	{ preference_id: 16, city_id: 16},
+	{ preference_id: 8, city_id: 11}
      ]);
 };
